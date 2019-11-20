a013ef5637447537ce89a7568be64fad3806a8bc
liubo <253620987@qq.com>
2019-11-20 11:29:52 +0800

    init

---
+++ curator-simple-config/src/main/java/com/wis/curator/config/CuratorSimpleConfigApplication.java
@@ -0,0 +1,13 @@
+package com.wis.curator.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuratorSimpleConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuratorSimpleConfigApplication.class, args);
+    }
+
+}
