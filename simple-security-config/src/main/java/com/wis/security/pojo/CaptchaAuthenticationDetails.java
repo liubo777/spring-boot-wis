de196cada57f212bab8c4077177bf70e7e27ceee
Mr LLL <535797316@qq.com>
2020-02-04 10:38:40 +0800

    添加序列化接口，解决将session保存到redis中出错的问题

--- simple-security-config/src/main/java/com/wis/security/pojo/CaptchaAuthenticationDetails.java
+++ simple-security-config/src/main/java/com/wis/security/pojo/CaptchaAuthenticationDetails.java
@@ -1,13 +1,15 @@
 package com.wis.security.pojo;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
 
 /**
  * 存储用来校验的验证码和答案
  * Created by liuBo
  * 2020/1/10.
  */
-public class CaptchaAuthenticationDetails {
+public class CaptchaAuthenticationDetails implements Serializable {
+    private static final long serialVersionUID = 3096728631204002587L;
     private final String answer;
     private final String captcha;
 
